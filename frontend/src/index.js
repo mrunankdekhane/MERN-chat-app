8238b3f08182f99a2f3504c1bb64546951b85269
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-04-29 00:34:10 +0530

    all files added

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ChatProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ChatProvider>
+  </BrowserRouter>,
+);
