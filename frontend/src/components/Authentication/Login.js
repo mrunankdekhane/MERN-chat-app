48009af6d8f0d1fd4e1c3f028a2fc1ece92756c7
Mrunank Dekhane <mrunankdekhane@gmail.com>
2026-05-02 02:50:51 +0530

    new changes on login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:5001/api/user/login",
+        "/api/user/login",
         { email, password },
         config,
       );
